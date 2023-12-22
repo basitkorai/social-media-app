ecf73ced31bc9659ee42557ff39d309aac15f75e
basitkorai <basitalikorai7@gmail.com>
2023-12-23 01:21:42 +0500

    feat: add loading animation

---
+++ src/components/Loading.jsx
@@ -0,0 +1,26 @@
+import CircularProgress from '@mui/material/CircularProgress'
+import { useAppContext } from '../context/context'
+import { Box } from '@mui/material'
+
+const Loading = () => {
+  const {
+    state: { isMode },
+  } = useAppContext()
+  return (
+    <Box
+      sx={{
+        width: '100vw',
+        height: '100vh',
+        display: 'grid',
+        position: 'fixed',
+        top: 0,
+        placeContent: 'center',
+        backgroundColor: isMode === 'dark' ? '#121212' : '#ffffff',
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  )
+}
+
+export default Loading
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Suspense, lazy } from 'react'
+const App = lazy(() => import('./App.jsx'))
 import ContextProvider from './context/context.jsx'
+import Loading from './components/Loading.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ContextProvider>
-      <App />
+      <Suspense fallback={<Loading />}>
+        <App />
+      </Suspense>
     </ContextProvider>
   </React.StrictMode>
 )
