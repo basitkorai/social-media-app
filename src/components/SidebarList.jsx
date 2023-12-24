3b8bb169fcc0079439e07af416c1ad1d3ade0f83
basitkorai <basitalikorai7@gmail.com>
2023-12-24 12:34:42 +0500

    fix: navlinks color is not in sync with theme

--- src/components/SidebarList.jsx
+++ src/components/SidebarList.jsx
@@ -4,21 +4,23 @@ import {
   ListItem,
   ListItemButton,
   ListItemIcon,
-  ListItemText,
 } from '@mui/material'
+import { useTheme } from '@mui/material'
 
 import { Link, NavLink } from 'react-router-dom'
 import navlinks from '../data/navlinks'
 
 const SidebarList = () => {
+  const { palette } = useTheme()
   return (
     <Box flex={1} p={2}>
       <List>
         {navlinks.map((link, index) => {
           return (
             <NavLink
+              key={index}
               style={{
-                color: 'white',
+                color: palette.text.primary,
                 textDecoration: 'none',
                 fontFamily: 'Inter, sans-serif',
                 padding: '0.5rem',
@@ -26,7 +28,6 @@ const SidebarList = () => {
               to={link.link}
             >
               <ListItem
-                key={index}
                 disablePadding
                 sx={{ borderRadius: '1rem', overflow: 'hidden' }}
               >
