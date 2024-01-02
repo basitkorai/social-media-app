fbf2d919c01aafbd1e35cafbcffe05614c607e0c
basitkorai <basitalikorai7@gmail.com>
2024-01-02 23:26:40 +0500

    fix: page title does not update on navigating

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 import Feed from '../components/Feed'
 import Rightbar from '../components/Rightbar'
 import Add from '../components/Add'
@@ -6,6 +7,10 @@ import Add from '../components/Add'
 const Home = ({ md }) => {
   return (
     <>
+      <Helmet>
+        <title>Home</title>
+        <meta name="description" content="Social Rocket's Home Page" />
+      </Helmet>
       <Feed />
       {md && <Rightbar />}
       <Add biggerScreen={md} />
