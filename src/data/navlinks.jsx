2d0d4e4baa0e448b80c637dce68c76bb85cee004
basitkorai <basitalikorai7@gmail.com>
2024-01-08 17:18:31 +0500

    refactor: add dynamic menu items

--- src/data/navlinks.jsx
+++ src/data/navlinks.jsx
@@ -3,7 +3,12 @@ import PersonIcon from '@mui/icons-material/Person'
 import ArticleIcon from '@mui/icons-material/Article'
 import StoreIcon from '@mui/icons-material/Store'
 import LogoutIcon from '@mui/icons-material/Logout'
+import { Article, Logout } from '@mui/icons-material'
 import { Bookmark } from '@mui/icons-material'
+import { Settings } from '@mui/icons-material'
+import AccountBoxIcon from '@mui/icons-material/AccountBox'
+
+const menuIconStyles = { marginRight: '0.5rem' }
 
 const navlinks = [
   {
@@ -37,5 +42,34 @@ const navlinks = [
     icon: <LogoutIcon color="primary" />,
   },
 ]
+
+const desktopMenuItems = [
+  {
+    name: 'Profile',
+    route: 'myprofile',
+    icon: <AccountBoxIcon sx={menuIconStyles} />,
+  },
+  { name: 'Settings', icon: <Settings sx={menuIconStyles} /> },
+]
+
+const mobileMenuItems = [
+  {
+    name: 'Pages',
+    route: 'pages',
+    icon: <Article sx={menuIconStyles} />,
+  },
+  {
+    name: 'Saved posts',
+    route: 'saved_posts',
+    icon: <Bookmark sx={menuIconStyles} />,
+  },
+  {
+    name: 'Logout',
+    route: 'login',
+    icon: <Logout sx={menuIconStyles} />,
+  },
+]
+
+export { desktopMenuItems, mobileMenuItems }
 
 export default navlinks
